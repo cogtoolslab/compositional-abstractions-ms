3ee57d0d98abc503ba05275de0f6e71a55118f11
Robert Hawkins <hawkrobe@gmail.com>
2023-08-01 08:15:45 -0500

    update model and param grid

--- model/convention_formation/webppl_implementation/input/gen_grid.py
+++ model/convention_formation/webppl_implementation/input/gen_grid.py
@@ -8,8 +8,10 @@ import numpy as np
 chainNum = 0
 with open('grid_49ppts.csv', 'w') as csv_file :
     writer = csv.writer(csv_file, delimiter=',')
-    for alpha in [1, 4, 16, 64] :
-        for beta in np.linspace(0, 1, 11) :
-            for participantNumber in range(1, 50) :
-                writer.writerow([chainNum, round(alpha, 1), round(beta, 1), participantNumber])
-                chainNum = chainNum + 1
+    for alpha in [5] :
+        for beta in np.linspace(0, .5, 6) :
+            for epsilon in np.linspace(0.001, .1, 5) :
+                for modelType in ['full', 'noDSL', 'noConvention']:
+                    for pid in range(1, 50) :
+                        writer.writerow([chainNum, round(alpha, 1), round(beta, 2), round(epsilon,3), pid, modelType])
+                        chainNum = chainNum + 1
--- model/convention_formation/webppl_implementation/run_model.sh
+++ model/convention_formation/webppl_implementation/run_model.sh
@@ -1,3 +1,3 @@
 # To run this script, paste the following line into terminal:
-parallel --bar --colsep ',' "sh ./run_model.sh {1} {2} {3} {4}" :::: input/grid_49ppts.csv
-webppl coordinate_DSL_pragmatic_speaker.wppl --require webppl-json --require webppl-csv -- --numIterations=2 --chainNum=$1 --alpha=$2 --beta=$3 --participantNumber=$4
+# parallel --bar --colsep ',' "sh ./run_model.sh {1} {2} {3} {4} {5} {6}" :::: input/grid_49ppts.csv
+webppl dual_coordination.wppl --require webppl-json --require webppl-csv -- --numIterations=1 --chainNum=$1 --alpha=$2 --beta=$3 --epsilon=$4 --participantNumber=$5 --modelType=$6
